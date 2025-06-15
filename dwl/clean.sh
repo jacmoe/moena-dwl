aad438400631fda94b9ab3c290fc1784a37b166e
jacmoe <jacmoe.dk@gmail.com>
2025-06-15 19:39:59 +0200

    Added clean script

---
+++ dwl/clean.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+rm -f config.h && 
+rm -f cursor-shape-v1-protocol.h &&
+rm -f pointer-constraints-unstable-v1-protocol.h &&
+rm -f wlr-layer-shell-unstable-v1-protocol.h &&
+rm -f wlr-output-power-management-unstable-v1-protocol.h &&
+rm -f xdg-shell-protocol.h
