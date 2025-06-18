3cba9b21ef3c021a0ce95d3a3e21528567c6418c
jacmoe <jacmoe.dk@gmail.com>
2025-06-18 22:12:45 +0200

    Mate-polkit works!

--- startdwl.sh
+++ startdwl.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-slstatus -s | dwl -s "exec dbus-update-activation-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP & 
-lxpolkit &"
+slstatus -s | dwl -s "exec dbus-update-activation-environment --systemd WAYLAND_DISPLAY DISPLAY XAUTHORITY XDG_CURRENT_DESKTOP & 
+exec /usr/libexec/polkit-mate-authentication-agent-1 &"
