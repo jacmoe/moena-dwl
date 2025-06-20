6b4419a53a12050a1604e7f9e7c99717b5ef6225
jacmoe <jacmoe.dk@gmail.com>
2025-06-21 00:36:00 +0200

    Startdwl script now uses wslstatus

--- startdwl.sh
+++ startdwl.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-slstatus -s | dwl -s "exec dbus-update-activation-environment --systemd WAYLAND_DISPLAY DISPLAY XAUTHORITY XDG_CURRENT_DESKTOP & 
+wslstatus -s | dwl -s "exec dbus-update-activation-environment --systemd WAYLAND_DISPLAY DISPLAY XAUTHORITY XDG_CURRENT_DESKTOP & 
 exec /usr/libexec/polkit-mate-authentication-agent-1 &"
